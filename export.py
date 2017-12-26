d323cc5eb8827fc2fb836cc2843b058bdc856ed7
apple1417 <theapple1417@gmail.com>
2017-12-27 00:03:44 +1300

    Automated exporting .gros

---
+++ export.py
@@ -0,0 +1,18 @@
+import os
+import shutil
+import sys
+
+BLACKLIST = shutil.ignore_patterns(".git*", "Plaintext Scripts*","export.py", "Randomizer.nfo", "MarkerNames.txt")
+
+if os.path.exists("Randomizer.gro"):
+  print("Removing existing .gro")
+  os.remove("Randomizer.gro")
+
+print("Copying files")
+shutil.copytree(os.path.dirname(sys.argv[0]), "temp/Content/Talos/Levels/Randomizer", ignore=BLACKLIST)
+shutil.copy2("Randomizer.nfo", "temp/Content/Talos/Levels/Randomizer.nfo")
+print("Zipping")
+shutil.make_archive("Randomizer", 'zip', "temp")
+shutil.move("Randomizer.zip", "Randomizer.gro")
+print("Removing temp directory")
+shutil.rmtree("temp")
