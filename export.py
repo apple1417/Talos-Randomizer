e63b34097632a399336f7c4712406a31473f5721
apple1417 <theapple1417@gmail.com>
2018-10-13 18:45:56 +1300

    translation file attempt
    the english version doesn't load, changing it to something like chinese makes it work though

--- export.py
+++ export.py
@@ -2,7 +2,8 @@ import os
 import shutil
 import sys
 
-BLACKLIST = shutil.ignore_patterns(".git*", "Plaintext Scripts*","*.py", "Randomizer.nfo", "MarkerNames.txt", "README.md")
+BLACKLIST = shutil.ignore_patterns(".git*", "Plaintext Scripts*","*.py", "Randomizer.nfo",
+                                   "MarkerNames.txt", "README.md", "Locales*")
 
 if os.path.exists("Randomizer.gro"):
   print("Removing existing .gro")
@@ -10,6 +11,7 @@ if os.path.exists("Randomizer.gro"):
 
 print("Copying files")
 shutil.copytree(os.path.dirname(sys.argv[0]), "temp/Content/Talos/Levels/Randomizer", ignore=BLACKLIST)
+# shutil.copytree("Locales", "temp/Content/Talos/Locales")
 shutil.copy2("Randomizer.nfo", "temp/Content/Talos/Levels/Randomizer.nfo")
 print("Zipping")
 shutil.make_archive("Randomizer", 'zip', "temp")
